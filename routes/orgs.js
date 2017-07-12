333c022a99da3eb0df94241c7f06517d0bea8639
Dan <dan.kirichok@gmail.com>
2017-07-11 20:02:56 -0400

    Added ability to leave an organization from the organization's page.

--- routes/orgs.js
+++ routes/orgs.js
@@ -4,10 +4,16 @@ var router = express.Router();
 var User = require('../models/user');
 var Organization = require('../models/organization');
 
+/*
+ * Displays page of queried organization
+ */
 router.get('/', ensureAuthenticated, function(req, res){
-	var org_code = req.query['org_code']
+	var org_code = req.query['org_code'];
 	Organization.getOrganizationByCode(org_code, function(err, org){
-		
+		/*
+		 * If organization exists, fetches
+		 * all of its data and loads page
+		 */
 		if (org){
 			var context = {
 				org: org,
@@ -28,6 +34,76 @@ router.get('/', ensureAuthenticated, function(req, res){
 	});
 });
 
+
+
+
+/*
+ * Leaves requested organization
+ */
+router.post('/leave_org', ensureAuthenticated, function(req, res){
+	var org_code = req.body.org_code;
+	
+	console.log(org_code);
+	
+	/*
+	 * Removes user as a member of the organization
+	 */
+	Organization.getOrganizationByCode(org_code, function(err, org){
+		if (err) throw err;
+		
+		var new_members = org.members;
+		
+		/*
+		 * Index of the user that wants to leave
+		 */
+		var user_index = new_members.indexOf(req.user.username);
+		
+		/*
+		 * Makes sure user is in members list
+		 * Should always be the case, just precautionary
+		 */
+		if (user_index >= 0){
+			new_members.splice(user_index, 1);
+		}
+		
+		
+		org.update({members: new_members}, throwError);
+	});
+	
+	
+	
+	/*
+	 * Removes organization from user orgs list
+	 */
+	 User.getUserByUsername(req.user.username, function(err, user){
+		if (err) throw err;
+		
+		var new_orgs = user.orgs;
+		 
+		/*
+		 * Index of the org  wanted to leave
+		 */
+		var org_index = new_orgs.indexOf(org_code);
+		
+		/*
+		 * Makes sure org is in orgs list
+		 * Should always be the case, just precautionary
+		 */
+		if (org_index >= 0){
+			new_orgs.splice(org_index, 1);
+		}
+		
+		user.update({orgs: new_orgs}, throwError);
+	 });
+	
+	req.flash('success_msg', 'You have successfully left the organization');
+	res.redirect('/');
+});
+
+
+
+
+
 function isCounselor(account_type){
 	if (account_type === "c"){
 		return true;
@@ -42,6 +118,11 @@ function isStudent(account_type){
 	return false;
 }
 
+
+
+
+
+
 function isStudentInOrg(members, username){
 	//indexOf does not work on internet explorer
 	if (members.indexOf(username) >= 0){
@@ -49,6 +130,21 @@ function isStudentInOrg(members, username){
 	}
 	return false;
 }
+
+
+
+
+
+/*
+ * I think this is only for development setting
+ * Simply throws an error if there is an error
+ */
+function throwError(err, result){
+	if (err) throw err;
+}
+
+
+
 
 
 /*
